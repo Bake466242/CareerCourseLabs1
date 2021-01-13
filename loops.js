69a3a10f1fcc6dc77847b78a5f80805d8e9eed1e
Rebecca Bakels <rebeccabakels@gmail.com>
2021-01-13 16:35:15 -0500

    worked on accessing JSON file

--- loops.js
+++ loops.js
@@ -1,5 +1,21 @@
-let students = ['Dennis', 'Becca', 'Bobby', 'Scar']
+// let students = ['Dennis', 'Becca', 'Bobby', 'Scar']
 
-for(let i = 0; i < students.length; i++){
-    console.log(students[i])
-}
+// for(let i = 0; i < students.length; i++){
+//     console.log(students[i])
+// }
+
+//
+
+// for (let i=0; i <15; i++ ){
+//     if (i % 2){
+//         console.log(`${i} is odd`)
+//     } else {
+//         console.log(`${i} is even`)
+//     }
+// }
+
+const num = [10, 30, 50, 75, 32]
+const num2 = ['tan', 'blue', 'grey']
+const num3 = num.concat(num2)
+console.log(num3)
+
---
+++ useExternal.js
@@ -0,0 +1,28 @@
+const coffees = require('./coffee.json')
+/*
+{
+    "title": "Black",
+    "description": "Black coffee is as simple as it gets with ground coffee beans steeped in hot water, served warm. And if you want to sound fancy, you can call black coffee by its proper name: cafe noir.",
+    "ingredients": [
+      "Coffee"
+    ],
+    "id": 1
+  }
+*/
+
+// let milkyCoffees = coffees.filter(recipe => {
+//    return recipe.ingredients.includes('Milk')
+// })
+
+
+//let milkyCoffees = coffees.filter( recipe =>recipe.ingredients.includes('Steamed Milk') )
+
+// coffees.forEach(recipe => {
+// if(recipe.ingredients.includes('Steamed Milk')
+// || recipe.ingredients.includes('1oz Steamed Milk')){
+//     console.log(recipe.id, recipe.title)
+// }
+// })
+
+coffees.forEach(recipe =>
+    recipe.ingredients.includes('Steamed Milk') && console.log(recipe.title))
