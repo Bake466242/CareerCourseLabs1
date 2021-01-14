55dbbebb61f7a8883295a35e187e6df25e6c4b15
Rebecca Bakels <rebeccabakels@gmail.com>
2021-01-14 16:07:33 -0500

    painters lab

--- arraylab.js
+++ arraylab.js
@@ -17,44 +17,73 @@ const painters = [
 
   // 2. give list of painters names and last names 
 
-  
-// function painterName(first, last){
-//     //let painter = first ++ last
-//     return console.log(`${first} ${last}`)
-// }
-// painterNames(painters.name, painters.lastName)
+//   let newArray = painters.map(painter => {
+//     return {
+//         name: painter.name,
+//         last: painter.lastName
+//      }
+//     })
+//     console.log(newArray)
+    
 
-
-  
   // 3. sort painters alphabetically by last names
 
-// const painterList = painters.sort(function (last)) {
-    
-//     return
-// }
+//   let sortedArray = painters.sort((a,b) => (a.lastName > b.lastName) ? 1 : -1) //ternary
+
+//   console.log(sortedArray)
 
   // 4. give list of painters that names or last names start with K?
   
+//   const namesK = painters.filter(painter => painter.name.startsWith('K') || painter.lastName.startsWith('K') );
+
+//   console.log(namesK);
 
 
   // 5. print names of painters that that are still alive
   // and their current age
 
-  const isAlive = painters.filter (painter => !painter.passed)
-  const theirAge = isAlive.map (artist => { 
-    var today = new Date().getFullYear()
-    var age = today - artist.born
-    return `${artist.name} is ${age}`
-  })
-  console.log(theirAge)
+//   const isAlive = painters.filter (painter => !painter.passed)
+
+//   const theirAge = isAlive.map (artist => { 
+//     //var today = new Date().getFullYear()
+//     var age = new Date().getFullYear() - artist.born
+//     return `${artist.name} is ${age}`
+//   })
+//   console.log(theirAge)
 
   
-  // 6. give list of painters that contain 'li' in their name 
+  // 6. give list of painters that contain 'li' in their last name 
+
+//   const namesLi = painters.filter(painter => painter.lastName.includes('li') );
+
+//     console.log(namesLi);
   
 
   // 7. sort painters by birth date, from oldest to youngest
   
+//   let sortedArray = painters.sort((a,b) => (a.born > b.born) ? 1 : -1) //ternary
+
+// console.log(sortedArray)
   
-  
+// 8. give list of strings of artists and their age when died or current if still alive
+ // ex: 'Gustav Klimt was 56 when passed', 'Kara Walker is 52'
+
+ let newPainters = painters.map(painter => {
+    if (painter.passed == true){
+        `${painters.name} ${painters.lastName} was ${age} when passed`
+    } else{
+        `${painters.name} ${painters.lastName} is ${age}`
+    }
+        })
+        console.log(newPainters)
+
+
+
+
+// painters.forEach(member => {
+// let prefix = (member.gender === 'male') ? 'sir' : 'lady' 
+// let greeting = `Hello ${prefix} ${member.name}`
+// console.log(greeting)
+// })
   
   
--- arraymethods.js
+++ arraymethods.js
@@ -6,16 +6,69 @@
 
 // console.log (kids.filter(kid => kid.age < 18))
 
-//.sort() 
+//------------------------------------------------------------------------
+
+// //.sort() 
+
+// let family = [
+//     {name: 'Sage', gender: 'female', age: 20 },
+//     {name: 'Noah', gender: 'male', age: 17 },
+//     {name: 'Henry', gender: 'male', age: 11 },
+//     {name: 'Todd', gender: 'male', age: 45 },
+//     {name: 'Meghann', gender: 'male', age: 43 },
+// ]
+
+// let sortedArray = family.sort((a,b) => (a.name > b.name) ? 1 : -1) //ternary
+
+// console.log(sortedArray)
+
+//------------------------------------------------------------------------
+//.forEach
+
+// let family = [
+//     {name: 'Sage', gender: 'female', age: 20 },
+//     {name: 'Noah', gender: 'male', age: 17 },
+//     {name: 'Henry', gender: 'male', age: 11 },
+//     {name: 'Todd', gender: 'male', age: 45 },
+//     {name: 'Meghann', gender: 'female', age: 43 },
+// ]
+
+
+// family
+// .filter(person => person.age < 21) //chain
+// .forEach(member => {
+// let prefix = (member.gender === 'male') ? 'sir' : 'lady' 
+// let greeting = `Hello ${prefix} ${member.name}`
+// console.log(greeting)
+// })
+
+// family.forEach(member => {
+//     if(person.age < 21) {
+//     let prefix = (member.gender === 'male') ? 'sir' : 'lady' 
+//     let greeting = `Hello ${prefix} ${member.name}`
+//     console.log(greeting)
+//     }
+//     })
+
+//------------------------------------------------------------------------
+//.map -want a new full array with different shape, get new info from the data provided
 
 let family = [
     {name: 'Sage', gender: 'female', age: 20 },
     {name: 'Noah', gender: 'male', age: 17 },
     {name: 'Henry', gender: 'male', age: 11 },
     {name: 'Todd', gender: 'male', age: 45 },
-    {name: 'Meghann', gender: 'male', age: 43 },
+    {name: 'Meghann', gender: 'female', age: 43 },
 ]
 
-let sortedArray = family.sort((a,b) => (a.name > b.name) ? 1 : -1)
+//I want each persons name and birth year
 
-console.log(sortedArray)
+let newFam = family.map(member => {
+let born = 2020 - member.age
+return {
+    name: member.name,
+    born: born
+ }
+})
+console.log(newFam)
+
