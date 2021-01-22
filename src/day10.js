9e70c7f3f30a1f89c5348b1b001f399b0ee635a0
Rebecca Bakels <rebeccabakels@gmail.com>
2021-01-22 10:23:46 -0500

    callbacks

---
+++ src/day10.js
@@ -0,0 +1,23 @@
+// function getName(callback){
+//     var name = ('Bobby')
+//     callback(name)
+//     }
+//     getName(res => console.log('Hello', res))
+
+let beefStack = [beef, beef2, beef3]
+
+// function getBeef(){
+//     return new promise((resolve, reject) => {
+//     if(beefStack.length){
+//         resolve(beefStack.pop())
+//     } else {
+//         reject('no beef!')
+//     }
+//     })
+// }
+
+async function getRecipe(CoffeeName){
+    await let myRecipe =
+    return myRecipe.ingrediants 
+}
+    
